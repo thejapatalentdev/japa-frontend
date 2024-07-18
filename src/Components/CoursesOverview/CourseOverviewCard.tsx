715637f9cb6cecf5de9f284e4348bc7fbc71b796
david waza <davidyega69@gmail.com>
2024-07-18 12:53:08 +0100

    feat: added course overview card

---
+++ src/Components/CoursesOverview/CourseOverviewCard.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Typography from "../Typography/Typography";
+import Imageview from "../Imageview/Imageview";
+import Button from "../Button/Button";
+
+const CourseOverviewCard = () => {
+  return (
+    <div>
+      <div className="border rounded-[16px] py-10 px-5 w-[60%] m-auto shadow-sm">
+        <Typography
+          variant="primary"
+          size="xl"
+          weight="bold"
+          className="uppercase"
+        >
+          Course Overview
+        </Typography>
+        <div className="my-5 border-2 w-20 border-[#5922A9] bg-[#5922A9] rounded-md"></div>
+        <div className="space-y-5">
+          <div className="flex gap-4">
+            <Imageview src="/assets/analytics-01.svg" width="w-full" />
+            <Typography variant="primary" size="sm" weight="medium">
+              Beginner to Pro
+            </Typography>
+          </div>
+          <div className="flex gap-4">
+            <Imageview src="/assets/diploma.svg" width="w-full" />
+            <Typography variant="primary" size="sm" weight="medium">
+              14 Lessons
+            </Typography>
+          </div>
+          <div className="pt-20">
+            <Button variant="primary" className="w-full">Enroll</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CourseOverviewCard;
--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,14 +1,17 @@
+import CourseOverviewCard from "@/Components/CoursesOverview/CourseOverviewCard";
 import CoursesOverview from "@/Components/CoursesOverview/CoursesOverview";
 import React from "react";
 
 const Courses = () => {
   return (
     <div>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3">
-        <section className="col-span-2 p-5 md:p-20">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 p-5 md:p-20">
+        <section className="col-span-2 ">
           <CoursesOverview />
         </section>
-        <section className="col-span-1"></section>
+        <section className="col-span-1">
+          <CourseOverviewCard />
+        </section>
       </div>
     </div>
   );
