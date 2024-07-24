18a8930d0f09af1b3768a199584fd8ad2e69fb0c
david waza <davidyega69@gmail.com>
2024-07-24 11:49:43 +0100

    local commits

--- src/Components/CoursesOverview/CourseOverviewCard.tsx
+++ src/Components/CoursesOverview/CourseOverviewCard.tsx
@@ -6,7 +6,7 @@ import Button from "../Button/Button";
 const CourseOverviewCard = () => {
   return (
     <div>
-      <div className="border rounded-[16px] py-10 px-5 w-[60%] m-auto shadow-sm">
+      <div className="border rounded-[16px] py-10 px-5 w-full lg:w-[60%] m-auto shadow-sm">
         <Typography
           variant="primary"
           size="xl"
@@ -30,7 +30,9 @@ const CourseOverviewCard = () => {
             </Typography>
           </div>
           <div className="pt-20">
-            <Button variant="primary" className="w-full">Enroll</Button>
+            <Button variant="primary" className="w-full">
+              Enroll
+            </Button>
           </div>
         </div>
       </div>
